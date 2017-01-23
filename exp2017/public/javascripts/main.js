d1517880f560279fe849ad3f53a32e292a5cd50a
Alexey Belkov <knots@ya.ru>
2017-01-23 20:56:07 +0300

    Исправлена загрузка jQuery

--- exp2017/public/javascripts/main.js
+++ exp2017/public/javascripts/main.js
@@ -1,5 +1,4 @@
-window.onload = (function(){
-	// alert( "You are running jQuery version: " + $.fn.jquery );
-var y = document.getElementById('chack');
-    console.log(y);
-}());
+$(function() {
+  $el = $('#chack');
+  console.log($el);
+});
